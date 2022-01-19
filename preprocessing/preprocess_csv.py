0428e2f43d8899c0e29146aff5d63ce8eb338ac7
ahmedmustahid <einsteinium25@gmail.com>
2022-01-19 18:11:17 +0900

    cleaned excel file,selected bad skin men

---
+++ preprocessing/preprocess_csv.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import os
+import pandas as pd
+
+def select_men_bad_skin(df):
+
+    df_new = df[(df["degradation rate"]=="5") & (df["gender"]=="m") ]
+    return df_new
+
+
+df = pd.read_csv("preprocessed_file.csv")
+df_new = select_men_bad_skin(df)
+#print(df_new)
+
+for _, row in df_new["patientNumber"].iteritems():
+    #print(row)
+    for root, dirs, files in os.walk("/home/ahmed/work/atopiWork/atopZips"):
+    
+        for directory in dirs:
+            if directory == str(row):
+                print("inside")
+                print(root)
+                dirlist = os.listdir(root+"/"+directory)
+                print(*dirlist,sep="\n")
+                break
